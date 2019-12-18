e987c0647d138a5302235d89b825b7b416461217
Hanul-Choi <han95210@naver.com>
2019-12-18 18:12:44 +0900

    문제 해결 1

--- src/JavaLevel1.java
+++ src/JavaLevel1.java
@@ -5,6 +5,9 @@ public class JavaLevel1 {
 		System.out.println("Hello World");
 		System.out.println("Hi World");
 		System.out.println("Hello Word");
+		System.out.println("해냈다 해냈어!!");
+		System.out.println("아 이런 너무 힘들고 어려웠다!!");
+		System.out.println("원인은 원격저장소보다 로컬저장소의 작업파일이 오래되어서 생기는 문제였다!!");
 
 	}
 
