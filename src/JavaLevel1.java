f62bc0b73f4cce66db6032e75acc37293111ed11
Hanul-Choi <han95210@naver.com>
2019-12-18 17:52:03 +0900

    git 1

---
+++ src/JavaLevel1.java
@@ -0,0 +1,11 @@
+
+public class JavaLevel1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hi World");
+		System.out.println("Hello Word");
+
+	}
+
+}
